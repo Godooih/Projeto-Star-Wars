108d34249d976c9b5aac191c1510ea068285806c
Thales Godoi <thalesgodoi1@gmail.com>
2025-04-03 21:26:40 -0300

    Projeto StarWars

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,21 @@
     <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/manage">Gerenciar Favoritos</router-link>
+      <h1>Olá, Vue está rodando!</h1>
     </nav>
     <router-view />
   </template>
   
-  <style scoped lang="scss">
-  nav {
-    display: flex;
-    gap: 20px;
-    background: blue;
-    padding: 10px;
-  
-    a {
-      color: grey;
-      text-decoration: none;
-      font-weight: bold;
-    }
-  }
-  </style>
+  <style>
+#app {
+  min-height: 100vh; /* Garante que ocupa toda a tela */
+  width: 100vw; /* Ocupa toda a largura */
+  background-color: gray;
+  color: white;
+  display: flex;
+  justify-content: center; /* Centraliza o conteúdo */
+  align-items: center; /* Centraliza verticalmente */
+}
+
+</style>
   
